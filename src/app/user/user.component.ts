446f5d59ed1e1ec04d88931ceb5c9e88da40e40d
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-10-29 14:20:48 +0100

    add-btn with tolltip user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import {CdkScrollable} from '@angular/cdk/scrolling';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CdkScrollable],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
+  positionOptions: TooltipPosition[] = ['above'];
+  position = new FormControl(this.positionOptions[0]);
 }
