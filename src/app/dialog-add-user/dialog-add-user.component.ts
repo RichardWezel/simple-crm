6d0fee7b94629790d50b7af24f9aa2f759e6aa5f
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-10-30 08:35:57 +0100

    added inputfields on dialog

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatIconModule,MatDatepickerModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
